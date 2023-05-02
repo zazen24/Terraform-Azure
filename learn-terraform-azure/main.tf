e521748e70775592b3aa2a266ba81f19caf620c7
shukche <64193092+shukche@users.noreply.github.com>
2023-05-02 21:52:25 +1000

    creating a new virtual network

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
-      
+
     }
   }
 
@@ -18,5 +18,14 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "australiaeast"
+}
+
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "australiaeast"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
