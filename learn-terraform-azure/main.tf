b326b1b0c36074bcbe4c84e7650f3293d8f49805
shukche <64193092+shukche@users.noreply.github.com>
2023-05-02 22:32:53 +1000

    corrections for var name

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "var.resource_group_name"
+  name     = var.resource_group_name
   location = "australiaeast"
 
   tags = {
