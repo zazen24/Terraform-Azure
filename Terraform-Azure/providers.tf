739b27147b191d2ccf70abc30e4ca60f0f3e9b00
shukche <64193092+shukche@users.noreply.github.com>
2023-09-09 19:39:52 +1000

    Configured right backend

--- Terraform-Azure/providers.tf
+++ Terraform-Azure/providers.tf
@@ -12,12 +12,12 @@ terraform {
       version = "~>3.0"
     }
   }
-#   backend "azurerm" {
+  backend "azurerm" {
 #       resource_group_name  = "tfstate"
 #       storage_account_name = "tfstateforchet"
 #       container_name       = tfstate-chet
-#       key                  = "terraform.tfstate"
-#   }
+      key                  = "terraform.tfstate"
+   }
  }
 
 provider "azurerm" {
