4566906ca874af71129851d9a85266cd060fd71d
zazen24 <chetan.shukla@outlook.com>
2024-05-13 13:13:51 +1000

    updates

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -27,8 +27,8 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   default_node_pool {
     name       = "default"
     node_count = "1"
-    vm_size    = "standard_d2_v2"
-    #vm_size    = "standard_B2s"
+    #vm_size    = "standard_d2_v2"
+    vm_size    = "standard_B2s"
   }
   network_profile {
     network_plugin    = "kubenet"
--- Terraform-Azure/providers.tf
+++ Terraform-Azure/providers.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      #version = "~>2.0"
+      version = "=3.0.0"
     }
     
     random = {
