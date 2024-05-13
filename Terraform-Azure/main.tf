ff27e74c94886fe4ae2de3fa8da98b2a769bf515
zazen24 <chetan.shukla@outlook.com>
2024-05-13 12:42:45 +1000

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -26,9 +26,9 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = "2"
-    #vm_size    = "standard_d2_v2"
-    vm_size    = "standard_B2s"
+    node_count = "1"
+    vm_size    = "standard_d2_v2"
+    #vm_size    = "standard_B2s"
   }
   network_profile {
     network_plugin    = "kubenet"
