5701c417b69118b1d1a637dcf1f915485a841d47
zazen24 <chetan.shukla@outlook.com>
2024-10-11 12:04:28 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -83,6 +83,11 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   identity {
     type = "SystemAssigned"
   }
+  addon_profile {
+    http_application_routing {
+      enabled = true
+    } 
+  } 
 }
 
 
@@ -96,71 +101,71 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
 
 # since these variables are re-used - a locals block makes this more maintainable
-locals {
-  backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
-  frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
-  http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
-  listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
-  request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
-  redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-}
+# locals {
+#   backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
+#   frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
+#   frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
+#   http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
+#   listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
+#   request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
+#   redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
+# }
 
-resource "azurerm_application_gateway" "network" {
-  name                = "appgatewayfork8"
-  resource_group_name = azurerm_resource_group.resource_group_AG.name
-  location            = azurerm_resource_group.resource_group_AG.location
+# resource "azurerm_application_gateway" "network" {
+#   name                = "appgatewayfork8"
+#   resource_group_name = azurerm_resource_group.resource_group_AG.name
+#   location            = azurerm_resource_group.resource_group_AG.location
 
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1
-  }
+#   sku {
+#     name     = "Standard_v2"
+#     tier     = "Standard_v2"
+#     capacity = 1
+#   }
 
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.AG_subnet.id
-  }
+#   gateway_ip_configuration {
+#     name      = "my-gateway-ip-configuration"
+#     subnet_id = azurerm_subnet.AG_subnet.id
+#   }
 
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
+#   frontend_port {
+#     name = local.frontend_port_name
+#     port = 80
+#   }
 
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
+#   frontend_ip_configuration {
+#     name                 = local.frontend_ip_configuration_name
+#     public_ip_address_id = azurerm_public_ip.example.id
+#   }
 
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
+#   backend_address_pool {
+#     name = local.backend_address_pool_name
+#   }
 
-  backend_http_settings {
-    name                  = local.http_setting_name
-    cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
+#   backend_http_settings {
+#     name                  = local.http_setting_name
+#     cookie_based_affinity = "Disabled"
+#     path                  = "/path1/"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 60
+#   }
 
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
+#   http_listener {
+#     name                           = local.listener_name
+#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
+#     frontend_port_name             = local.frontend_port_name
+#     protocol                       = "Http"
+#   }
 
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    priority                   = 9
-    rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
-  }
-}
+#   request_routing_rule {
+#     name                       = local.request_routing_rule_name
+#     priority                   = 9
+#     rule_type                  = "Basic"
+#     http_listener_name         = local.listener_name
+#     backend_address_pool_name  = local.backend_address_pool_name
+#     backend_http_settings_name = local.http_setting_name
+#   }
+# }
 
 # resource "azurerm_role_assignment" "example" {
 #   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
