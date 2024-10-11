3e5caafd1cf001b935224abfa009e934c8ad82ab
zazen24 <chetan.shukla@outlook.com>
2024-10-11 14:00:00 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -1,10 +1,6 @@
-# resource "random_pet" "rg_name" {
-#   prefix = var.resource_group_name_prefix
-# }
-
 resource "azurerm_resource_group" "rg" {
   location = "eastus2"
-  name     = "AKS-ResourceGroup"
+  name     = "Azure-ResourceGroup"
 
 }
 
