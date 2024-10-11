580471f80cb2b546d8ed6bdc02ac291240618f72
zazen24 <chetan.shukla@outlook.com>
2024-10-11 12:47:24 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -83,11 +83,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   identity {
     type = "SystemAssigned"
   }
-  addon_profile {
-    http_application_routing {
-      enabled = true
-    } 
-  } 
+  
 }
 
 
