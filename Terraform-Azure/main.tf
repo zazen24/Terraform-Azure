5520d53d8674f6f35164de6b70811ac5d419d191
zazen24 <chetan.shukla@outlook.com>
2024-10-16 10:59:42 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -38,15 +38,6 @@ resource "azurerm_subnet" "AKS_subnet" {
 }
 
 
-# resource "azurerm_public_ip" "AKS_subnet" {
-#   name                = "app-gateway-ip"
-#   location            = azurerm_resource_group.ag-rg.location
-#   resource_group_name = azurerm_resource_group.ag-rg.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
-
-
 resource "azurerm_container_registry" "acr" {
   name                = "DemooACRchet"
   resource_group_name = azurerm_resource_group.rg.name
@@ -104,67 +95,3 @@ resource "azurerm_role_assignment" "example" {
 output "aks_uai_appgw_object_id" {
   value = azurerm_kubernetes_cluster.cluster.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
 }
-
-
-# resource "azurerm_application_gateway" "network" {
-#   name                = "appgatewayfork8"
-#   resource_group_name = azurerm_resource_group.resource_group_AG.name
-#   location            = azurerm_resource_group.resource_group_AG.location
-
-#   sku {
-#     name     = "Standard_v2"
-#     tier     = "Standard_v2"
-#     capacity = 1
-#   }
-
-#   gateway_ip_configuration {
-#     name      = "my-gateway-ip-configuration"
-#     subnet_id = azurerm_subnet.AG_subnet.id
-#   }
-
-#   frontend_port {
-#     name = local.frontend_port_name
-#     port = 80
-#   }
-
-#   frontend_ip_configuration {
-#     name                 = local.frontend_ip_configuration_name
-#     public_ip_address_id = azurerm_public_ip.example.id
-#   }
-
-#   backend_address_pool {
-#     name = local.backend_address_pool_name
-#   }
-
-#   backend_http_settings {
-#     name                  = local.http_setting_name
-#     cookie_based_affinity = "Disabled"
-#     path                  = "/path1/"
-#     port                  = 80
-#     protocol              = "Http"
-#     request_timeout       = 60
-#   }
-
-#   http_listener {
-#     name                           = local.listener_name
-#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
-#     frontend_port_name             = local.frontend_port_name
-#     protocol                       = "Http"
-#   }
-
-#   request_routing_rule {
-#     name                       = local.request_routing_rule_name
-#     priority                   = 9
-#     rule_type                  = "Basic"
-#     http_listener_name         = local.listener_name
-#     backend_address_pool_name  = local.backend_address_pool_name
-#     backend_http_settings_name = local.http_setting_name
-#   }
-# }
-
-# resource "azurerm_role_assignment" "example" {
-#   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
-#   role_definition_name             = "AcrPull"
-#   scope                            = azurerm_container_registry.acr.id
-#   skip_service_principal_aad_check = true
-# }
