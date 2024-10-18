41a92637ea62df89c0421a28bd786936ae80abaa
zazen24 <chetan.shukla@outlook.com>
2024-10-18 14:10:49 +1100

    update acrpull role

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -71,6 +71,14 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     type = "SystemAssigned"
   }
 }
+
+
+resource "azurerm_role_assignment" "example-acr" {
+  principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
 #   ingress_application_gateway {
 #     gateway_name = "appgatewayfork8"
 #     subnet_id    = azurerm_subnet.AG_subnet.id
