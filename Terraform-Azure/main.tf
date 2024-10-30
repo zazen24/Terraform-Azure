f5a0edf4aa99fc0299e0e0e8799c137c34c2199b
zazen24 <chetan.shukla@outlook.com>
2024-10-30 16:48:06 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -81,6 +81,7 @@ resource "azurerm_key_vault" "example" {
 }
 
 
+
 # Create keyvault access policies for your user account and the terraform service principal.
 resource "azurerm_key_vault_access_policy" "kvap_service_principal" {
   key_vault_id            = azurerm_key_vault.example.id
