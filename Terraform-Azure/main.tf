3de64439d12ad8967496c48b3e581337678965aa
zazen24 <chetan.shukla@outlook.com>
2024-10-30 12:18:44 +1100

    create kv

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -46,36 +46,39 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
+data "azurerm_client_config" "current" {
+}
 
 
-# resource "azurerm_key_vault" "example" {
-#   name                        = "examplekeyvault"
-#   location                    = azurerm_resource_group.rg.location
-#   resource_group_name         = azurerm_resource_group.rg.name
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = var.tenant_id
-#   soft_delete_retention_days  = 7
-#   purge_protection_enabled    = false
 
-#   sku_name = "standard"
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
-  # access_policy {
-  #   tenant_id = var.tenant_id
-  #   object_id = c2b8e0f1-4dca-454a-8d88-d789bedaa56b
+  sku_name = "standard"
 
-  #   key_permissions = [
-  #     "Get",
-  #   ]
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-  #   secret_permissions = [
-  #     "Get",
-  #   ]
+    key_permissions = [
+      "Get",
+    ]
 
-  #   storage_permissions = [
-  #     "Get",
-  #   ]
-  # }
-##}
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
 
 # resource "azurerm_kubernetes_cluster" "cluster" {
 #   name                = "k8scluster"
