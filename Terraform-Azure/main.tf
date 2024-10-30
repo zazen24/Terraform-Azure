3a733c0dc7106c6bae6459d93dda2896e1b2f1d3
zazen24 <chetan.shukla@outlook.com>
2024-10-30 12:54:36 +1100

    Update main.tf

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -80,6 +80,25 @@ resource "azurerm_key_vault" "example" {
   # }
 }
 
+
+# Create keyvault access policies for your user account and the terraform service principal.
+resource "azurerm_key_vault_access_policy" "kvap_admin_users" {
+  key_vault_id            = azurerm_key_vault.example.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = data.azurerm_client_config.current.object_id
+  key_permissions = [
+    "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"
+  ]
+
+  secret_permissions = [
+     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+    ]
+
+  certificate_permissions = [
+   "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update"
+  ]
+}
+
 # resource "azurerm_kubernetes_cluster" "cluster" {
 #   name                = "k8scluster"
 #   location            = azurerm_resource_group.resource_group_AKS.location
