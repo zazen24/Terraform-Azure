2e65f2f8af466428b2b02669867e632fee576fc9
zazen24 <chetan.shukla@outlook.com>
2024-10-30 11:51:04 +1100

    Testing - remove KV

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -48,16 +48,16 @@ resource "azurerm_container_registry" "acr" {
 
 
 
-resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = var.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+# resource "azurerm_key_vault" "example" {
+#   name                        = "examplekeyvault"
+#   location                    = azurerm_resource_group.rg.location
+#   resource_group_name         = azurerm_resource_group.rg.name
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = var.tenant_id
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = false
 
-  sku_name = "standard"
+#   sku_name = "standard"
 
   # access_policy {
   #   tenant_id = var.tenant_id
@@ -75,7 +75,7 @@ resource "azurerm_key_vault" "example" {
   #     "Get",
   #   ]
   # }
-}
+##}
 
 # resource "azurerm_kubernetes_cluster" "cluster" {
 #   name                = "k8scluster"
