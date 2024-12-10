02b9443c2afbcafaf7d525593cf81872d1a7edee
ChetanS <chetan.shukla@outlook.com>
2024-12-10 16:54:40 +1100

    deploy

--- Terraform-Azure/fastapi/app/database.py
+++ Terraform-Azure/fastapi/app/database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 ##SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:Maverick12345!@34.132.89.61/fastapi"
 
