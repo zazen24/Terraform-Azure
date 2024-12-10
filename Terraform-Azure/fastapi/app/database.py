bb31ca50fb4f31ef014c9214d1ca4ec7a1f96e8a
ChetanS <chetan.shukla@outlook.com>
2024-12-10 15:47:29 +1100

    updated local repo

--- Terraform-Azure/fastapi/app/database.py
+++ Terraform-Azure/fastapi/app/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:Maverick12345!@postgresql-chetan.postgres.database.azure.com/fastapi"
+##SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:Maverick12345!@34.132.89.61/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
