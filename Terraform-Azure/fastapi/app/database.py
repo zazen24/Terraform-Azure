7fa03e4a95e474fe18c1944faa3e35f9fbfb8310
ChetanS <chetan.shukla@outlook.com>
2024-12-10 16:29:30 +1100

    introducing vars

--- Terraform-Azure/fastapi/app/database.py
+++ Terraform-Azure/fastapi/app/database.py
@@ -7,6 +7,22 @@ from sqlalchemy.orm import sessionmaker
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 ##SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:Maverick12345!@34.132.89.61/fastapi"
 
+with open('/etc/secrets/POSTGRES_HOST', 'r') as f:
+    postgres_host = f.read().strip()
+
+with open('/etc/secrets/POSTGRES_USER', 'r') as f:
+    postgres_user = f.read().strip()
+
+with open('/etc/secrets/POSTGRES_PASSWORD', 'r') as f:
+    postgres_password = f.read().strip()
+
+with open('/etc/secrets/POSTGRES_DB', 'r') as f:
+    postgres_db = f.read().strip()
+
+# Construct the SQLAlchemy Database URL dynamically
+SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}/{postgres_db}"
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
