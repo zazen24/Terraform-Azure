2a1b2f234b563c316c763ff7208b292287a6b5b5
ChetanS <chetan.shukla@outlook.com>
2025-02-13 16:35:47 +1100

    Secret changes

--- Terraform-Azure/fastapi/app/database.py
+++ Terraform-Azure/fastapi/app/database.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import sessionmaker
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 ##SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:Maverick12345!@34.132.89.61/fastapi"
 
-with open('/etc/secrets/POSTGRES_HOST', 'r') as f:
+with open('/etc/secrets/POSTGRES-HOST', 'r') as f:
     postgres_host = f.read().strip()
 
-with open('/etc/secrets/POSTGRES_USER', 'r') as f:
+with open('/etc/secrets/POSTGRES-USER', 'r') as f:
     postgres_user = f.read().strip()
 
-with open('/etc/secrets/POSTGRES_PASSWORD', 'r') as f:
+with open('/etc/secrets/POSTGRES-PASSWORD', 'r') as f:
     postgres_password = f.read().strip()
 
-with open('/etc/secrets/POSTGRES_DB', 'r') as f:
+with open('/etc/secrets/POSTGRES-DB', 'r') as f:
     postgres_db = f.read().strip()
 
 # Construct the SQLAlchemy Database URL dynamically
