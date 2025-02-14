648b2370e7cbf3e4dd3ecbd9e297c2d744c9ef86
ChetanS <chetan.shukla@outlook.com>
2025-02-14 14:02:33 +1100

    test

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -213,13 +213,13 @@ resource "azurerm_key_vault_access_policy" "kvap_admin_users" {
 }
 
 
-resource "helm_release" "secrets-store-csi" {
-  name       = "secrets-store-csi-driver"
-  namespace  = "kube-system"
-  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  chart      = "secrets-store-csi-driver"
-  #version    = "0.0.18"
-}
+# resource "helm_release" "secrets-store-csi" {
+#   name       = "secrets-store-csi-driver"
+#   namespace  = "kube-system"
+#   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+#   chart      = "secrets-store-csi-driver"
+#   #version    = "0.0.18"
+# }
 
 
 resource "helm_release" "azure-keyvault-provider" {
