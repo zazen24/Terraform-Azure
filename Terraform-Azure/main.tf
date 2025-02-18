05bf50b872045a43e6be9c99700218387a66cb31
ChetanS <chetan.shukla@outlook.com>
2025-02-18 14:19:52 +1100

    update

--- Terraform-Azure/main.tf
+++ Terraform-Azure/main.tf
@@ -158,7 +158,7 @@ resource "azurerm_key_vault" "example" {
 resource "azurerm_key_vault_access_policy" "kubernetes_cluster" {
   key_vault_id            = azurerm_key_vault.example.id
   tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].client_id    ### Changed to client id from object id #####
+  object_id               = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id 
   #object_id               =  azurerm_kubernetes_cluster.cluster.identity[0].principal_id
   key_permissions = [
      "Get"
